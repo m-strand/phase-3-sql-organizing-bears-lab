05bc170c8f744707c1d651c80f11af021dc99b0f
Brooke <brooke.strand007@gmail.com>
2022-09-13 17:22:07 -0400

    commit tree 0bcf60f0914d35f2e4eaad6f6fba73b1900805b1

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (1, "Mr. Chocolate", 50, "M", "brown", "lazy", false);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (2, "Rowdy", 16, "M", "Black", "rowdy", true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (3, "Tabitha", 27, "F", "White", "burden to her parents", true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (4, "Sergeant Brown", 80, "M", "blue", "crabby", false);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (5, "Melissa", 45, "F", "brown", "laidback", true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (6, "Grinch", 34, "M", "green", "lovely", true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (7, "Wendy", 11, "F", "pink", "friendly", true);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (8, null, 1, "M", "brown", "sleepy", true);
